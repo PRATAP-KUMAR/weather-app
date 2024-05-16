6ed4b4b77527d4d59dde583bd04120f672a6345f
PRATAP-KUMAR <pratap@fastmail.fm>
2024-05-16 20:58:58 +0530

    fix deploy

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -13,7 +13,9 @@ export const useFetchData = () => {
             setError(null);
             setData(null);
             setIsLoading(true);
-            const response = await fetch(`${URL}?q=${city}&appid=${APP_ID}`);
+            const response = await fetch(`${URL}?q=${city}&appid=${APP_ID}`, {
+                mode: "cors"
+            });
             const json = await response.json();
             if (json.cod === 200) {
                 setData(json);
